5099409842e04b013aa6ee4cc37e5aa6d0f7516d
Richard Holmboe <richard@dynamist.se>
2020-02-22 17:28:16 +0100

    Added more tests for core.update function

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,11 +5,48 @@ import os
 
 # sgit imports
 from sgit.core import Sgit, DEFAULT_REPO_CONTENT
-from sgit.exceptions import *
+from sgit.exceptions import SgitException, SgitConfigException
 
 # 3rd party imports
 import pytest
+from git import Git, Repo
 from ruamel import yaml
+
+
+def user_data():
+    working_gitname = 'working'
+    working_giturl = 'git+https://github.com/dynamist/sgit.git'
+    working_gitrev = 'master'
+
+    branch_gitname = 'broken-branch'
+    branch_giturl = 'git+https://github.com/dynamist/sgit.git'
+    branch_gitrev = 'branch-doesnt-exist'
+
+    non_working_gitname = 'broken-url'
+    non_working_giturl = 'git+https://github.com/dynamist/sgit-nonworking.git'
+    non_working_gitrev = 'master'
+
+    repository_test_data = {
+        working_gitname: {
+            'clone-url': working_giturl,
+            'revision': {
+                'branch': working_gitrev,
+            }
+        },
+        branch_gitname: {
+            'clone-url': branch_giturl,
+            'revision': {
+                'branch': branch_gitrev,
+            }
+        },
+        non_working_gitname: {
+            'clone-url': non_working_giturl,
+            'revision': {
+                'branch': non_working_gitrev,
+            }
+        }
+    }
+    return repository_test_data
 
 
 def test_class_create(sgit):
@@ -197,3 +234,95 @@ def test_repo_rename(sgit):
     config = sgit._get_config_file()
     assert 'foobar' not in config['repos']
     assert 'barfoo' in config['repos']
+
+def test_repo_update(sgit, mocker):
+    retcode = sgit.init_repo()
+    assert retcode == None
+
+    # Update with no arguments should raise TypeError
+    with pytest.raises(TypeError) as pytest_wrapped_e:
+        sgit.update()
+    assert pytest_wrapped_e.type == TypeError
+
+    # Update with no answer should return 1
+    mocker.patch('builtins.input', return_value='no')
+    retcode = sgit.update('all')
+    assert retcode == 1
+
+
+def test_repo_update_all(sgit, mocker, monkeypatch):
+    def mock_clone(*args, **kwargs):
+        switcher = {
+            'working': True,
+            'broken-url': Exception('Broken url'),
+            'broken-branch': Exception('Broken branch')
+        }
+        ret = switcher.get(os.path.basename(args[1]), Exception('mock_clone exception'))
+
+        if isinstance(ret, Exception):
+            raise ret
+        else:
+            return ret
+
+    retcode = sgit.init_repo()
+    assert retcode == None
+
+    # Patch input to yes further on
+    mocker.patch('builtins.input', return_value='yes')
+
+    # Update with 'all' should return SgitconfigException with empty config
+    with pytest.raises(SgitConfigException) as pytest_wrapped_e:
+        sgit.update('all')
+    assert pytest_wrapped_e.type == SgitConfigException
+
+    mocker.patch('builtins.input', return_value='yes')
+    monkeypatch.setattr(Repo, "clone_from", mock_clone)
+
+    # Update 'all' should return SgitException with faulty input
+    data = {'repos': {'broken-url': user_data().get('broken-url')}}
+    sgit.sgit_config_file_path.write(data)
+
+    with pytest.raises(SgitException) as pytest_wrapped_e:
+        sgit.update('all')
+    assert pytest_wrapped_e.type == SgitException
+    del data
+
+    # Update 'all' should return xx with correct input
+    # TODO: more update all tests
+
+def test_repo_update_named(sgit, mocker):
+    retcode = sgit.init_repo()
+    assert retcode == None
+
+    # Update with 'test' should return 1
+    retcode = sgit.update('test')
+    assert retcode == 1
+
+    # TODO: more named update tests
+
+
+def test_repo_update_list(sgit, mocker):
+    retcode = sgit.init_repo()
+    assert retcode == None
+
+    # Update with list should return 1
+    data = list(user_data().keys())
+    retcode = sgit.update(data)
+    assert retcode == 1
+    del data
+
+    # TODO: more update list() tests
+
+
+def test_repo_update_dict(sgit, mocker):
+    retcode = sgit.init_repo()
+    assert retcode == None
+
+    # Update with dict should raise TypeError
+    data = user_data().keys()
+    with pytest.raises(TypeError) as pytest_wrapped_e:
+        sgit.update(data)
+    assert pytest_wrapped_e.type == TypeError
+    del data
+
+    # TODO: more update dict() tests
