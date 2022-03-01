064efc4b1126297fc48da405167ee2017af19842
Johan Andersson <johan@dynamist.se>
2022-03-01 13:11:21 +0100

    Update all references and usage of old sgit to new subgit in all python code and related documentation. Pytest and local dev installation now works as expected.

--- subgit/exceptions.py
+++ subgit/exceptions.py
@@ -1,21 +1,21 @@
-# Base exception class for all Sgit related exceptions
-class SgitException(Exception):
+# Base exception class for all SubGit related exceptions
+class SubGitException(Exception):
     pass
 
 
-# Exceptions that is realted to the ".sgit.yml" config file and the contents in it
-class SgitConfigException(SgitException):
+# Exceptions that is realted to the ".subgit.yml" config file and the contents in it
+class SubGitConfigException(SubGitException):
     pass
 
 
 # Exceptions that happen related to some error with the git repo itself when
 # we attempt to make an operation on it.
-class SgitRepoException(SgitException):
+class SubGitRepoException(SubGitException):
     pass
 
 
 __all__ = [
-    "SgitException",
-    "SgitConfigException",
-    "SgitRepoException",
+    "SubGitException",
+    "SubGitConfigException",
+    "SubGitRepoException",
 ]
